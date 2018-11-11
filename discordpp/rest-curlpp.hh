f16ff90b53cb36b95631bc2b8deadbe70648e94c
danielbatterystapler <danielbatterystapler@gmail.com>
2018-11-11 15:22:46 -0500

    fix spacing around ifs and catches

--- discordpp/rest-curlpp.hh
+++ discordpp/rest-curlpp.hh
@@ -23,8 +23,7 @@ namespace discordpp{
         RestCurlPPModule(std::shared_ptr<asio::io_service> asio_ios, const std::string &token):
                 RestModule(asio_ios, token){}
         json call(std::string target, std::string token, json body, std::string requestType){
-            try
-            {
+            try {
                 std::stringstream outstream;
 
                 cURLpp::Cleanup clean;
@@ -35,18 +34,18 @@ namespace discordpp{
                 request.setOpt<curlpp::options::Url>("https://discordapp.com/api" + target);
                 request.setOpt(curlpp::options::Verbose(false));
 
-                if(!requestType.empty()) {
+                if (!requestType.empty()) {
                     request.setOpt(curlpp::options::CustomRequest(requestType));
                 }
 
                 std::list<std::string> header;
                 header.push_back("Content-Type: application/json");
-                if(token != "") {
-                    header.push_back(std::string("Authorization: ") + token);\
+                if (token != "") {
+                    header.push_back(std::string("Authorization: ") + token);
                 }
                 request.setOpt(curlpp::options::HttpHeader(header));
 
-                if(!body.empty()) {
+                if (!body.empty()) {
                     //std::cout << attachJSON.dump() << std::endl;
                     request.setOpt(curlpp::options::PostFields(body.dump()));
                     request.setOpt(curlpp::options::PostFieldSize(body.dump().length()));
@@ -60,22 +59,22 @@ namespace discordpp{
                     //std::cout << returned.dump() << std::endl;
                     std::string message = returned["message"].get<std::string>();
                     //std::cout << returned.dump() << std::endl;
-                    if(message == "You are being rate limited."){
+                    if (message == "You are being rate limited.") {
                         throw (ratelimit){returned["retry_after"].get<int>()};
                         //std::this_thread::sleep_for(std::chrono::milliseconds(returned["retry_after"].get<int>()));
-                    }else if(message != "") {
+                    } else if (message != "") {
                         std::cout << "Discord API sent a message: \"" << message << "\"" << std::endl;
                     }
-                } catch ( std::out_of_range & e) {
+                } catch (std::out_of_range & e) {
 
-                } catch ( std::domain_error & e) {
+                } catch (std::domain_error & e) {
 
                 }
 
                 return returned;
-            } catch ( curlpp::LogicError & e ) {
+            } catch (curlpp::LogicError & e) {
                 std::cout << "logic " << e.what() << std::endl;
-            } catch ( curlpp::RuntimeError & e ) {
+            } catch (curlpp::RuntimeError & e) {
                 std::cout << "runtime " << e.what() << std::endl;
             }
 
@@ -83,8 +82,7 @@ namespace discordpp{
         }
 
         json callWithFile(std::string target, std::string token, json body, std::string fileName, std::string requestType){
-            try
-            {
+            try {
                 std::stringstream outstream;
 
                 cURLpp::Cleanup clean;
@@ -95,19 +93,19 @@ namespace discordpp{
                 request.setOpt<curlpp::options::Url>("https://discordapp.com/api" + target);
                 request.setOpt(curlpp::options::Verbose(false));
 
-                if(!requestType.empty()) {
+                if (!requestType.empty()) {
                     request.setOpt(curlpp::options::CustomRequest(requestType));
                 }
 
                 std::list<std::string> header;
-                if(token != "") {
-                    header.push_back(std::string("Authorization: ") + token);\
+                if (token != "") {
+                    header.push_back(std::string("Authorization: ") + token);
                 }
                 request.setOpt(curlpp::options::HttpHeader(header));
 
                 {//add the forms parts, which includes the json and file
                     curlpp::Forms formParts;
-                    if(!body.empty()){
+                    if (!body.empty()) {
                         formParts.push_back(new curlpp::FormParts::Content("payload_json", body.dump()));
                     }
                     formParts.push_back(new curlpp::FormParts::File("file", fileName, "application/octet-stream"));
@@ -123,22 +121,22 @@ namespace discordpp{
                     //std::cout << returned.dump() << std::endl;
                     std::string message = returned["message"].get<std::string>();
                     //std::cout << returned.dump() << std::endl;
-                    if(message == "You are being rate limited."){
+                    if (message == "You are being rate limited.") {
                         throw (ratelimit){returned["retry_after"].get<int>()};
                         //std::this_thread::sleep_for(std::chrono::milliseconds(returned["retry_after"].get<int>()));
-                    }else if(message != "") {
+                    } else if (message != "") {
                         std::cout << "Discord API sent a message: \"" << message << "\"" << std::endl;
                     }
-                } catch ( std::out_of_range & e) {
+                } catch (std::out_of_range & e) {
 
-                } catch ( std::domain_error & e) {
+                } catch (std::domain_error & e) {
 
                 }
 
                 return returned;
-            } catch ( curlpp::LogicError & e ) {
+            } catch (curlpp::LogicError & e) {
                 std::cout << "logic " << e.what() << std::endl;
-            } catch ( curlpp::RuntimeError & e ) {
+            } catch (curlpp::RuntimeError & e) {
                 std::cout << "runtime " << e.what() << std::endl;
             }
 
