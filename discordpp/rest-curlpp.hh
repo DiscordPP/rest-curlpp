c1c48767e068298d3b5c35752519eef7160b8f17
danielbatterystapler <danielbatterystapler@gmail.com>
2018-11-11 23:12:57 -0500

    merge callWithFile into call

--- discordpp/rest-curlpp.hh
+++ discordpp/rest-curlpp.hh
@@ -22,7 +22,7 @@ namespace discordpp{
     public:
         RestCurlPPModule(std::shared_ptr<asio::io_service> asio_ios, const std::string &token):
                 RestModule(asio_ios, token){}
-        json call(std::string target, std::string token, json body, std::string requestType){
+        json call(std::string target, std::string token, json body, std::string requestType, std::string fileName){
             try {
                 std::stringstream outstream;
 
@@ -45,65 +45,13 @@ namespace discordpp{
                 }
                 request.setOpt(curlpp::options::HttpHeader(header));
 
-                if (!body.empty()) {
-                    //std::cout << attachJSON.dump() << std::endl;
-                    request.setOpt(curlpp::options::PostFields(body.dump()));
-                    request.setOpt(curlpp::options::PostFieldSize(body.dump().length()));
-                }
-
-                request.perform();
-
-                json returned = json::parse(outstream.str());
-
-                try {
-                    //std::cout << returned.dump() << std::endl;
-                    std::string message = returned["message"].get<std::string>();
-                    //std::cout << returned.dump() << std::endl;
-                    if (message == "You are being rate limited.") {
-                        throw (ratelimit){returned["retry_after"].get<int>()};
-                        //std::this_thread::sleep_for(std::chrono::milliseconds(returned["retry_after"].get<int>()));
-                    } else if (message != "") {
-                        std::cout << "Discord API sent a message: \"" << message << "\"" << std::endl;
+                if (fileName.empty()){
+                    if (!body.empty()) {
+                        //std::cout << attachJSON.dump() << std::endl;
+                        request.setOpt(curlpp::options::PostFields(body.dump()));
+                        request.setOpt(curlpp::options::PostFieldSize(body.dump().length()));
                     }
-                } catch (std::out_of_range & e) {
-
-                } catch (std::domain_error & e) {
-
-                }
-
-                return returned;
-            } catch (curlpp::LogicError & e) {
-                std::cout << "logic " << e.what() << std::endl;
-            } catch (curlpp::RuntimeError & e) {
-                std::cout << "runtime " << e.what() << std::endl;
-            }
-
-            return {};
-        }
-
-        json callWithFile(std::string target, std::string token, json body, std::string fileName, std::string requestType){
-            try {
-                std::stringstream outstream;
-
-                cURLpp::Cleanup clean;
-                //curlpp::Cleanup clean;
-                curlpp::Easy request;
-                curlpp::options::WriteStream ws(&outstream);
-                request.setOpt(ws);
-                request.setOpt<curlpp::options::Url>("https://discordapp.com/api" + target);
-                request.setOpt(curlpp::options::Verbose(false));
-
-                if (!requestType.empty()) {
-                    request.setOpt(curlpp::options::CustomRequest(requestType));
-                }
-
-                std::list<std::string> header;
-                if (token != "") {
-                    header.push_back(std::string("Authorization: ") + token);
-                }
-                request.setOpt(curlpp::options::HttpHeader(header));
-
-                {//add the forms parts, which includes the json and file
+                } else {
                     curlpp::Forms formParts;
                     if (!body.empty()) {
                         formParts.push_back(new curlpp::FormParts::Content("payload_json", body.dump()));
